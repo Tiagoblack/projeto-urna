8dd5cb0f2412d8676e242c17d3ad19e865c85109
Tiago dos Santos da Silva <xlzthiago@gmail.com>
2021-09-01 10:28:23 -0300

    projeto finalizado com sucesso

--- script.js
+++ script.js
@@ -12,15 +12,18 @@ let buttons = document.querySelectorAll('.btn');
 let etapaAtual = 0;
 let candidatoEscolhido;
 let numero;
+let branco = true;
+votos = [];
 
-// funções globais
+
 
 // funçao das etapas inicial
 
 const initionEtapas = () => {
     let etapa = etapas[etapaAtual];
-
     let numeroHtml = '';
+    numero = '';
+    branco = false
 
     for (let i = 0; i < etapa.numeros; i++) {
         if (i === 0) {
@@ -41,19 +44,53 @@ const initionEtapas = () => {
 
 }
 
-// função atualiza interface
+// funcões atualiza interface
 const fucntions = {
     CONFIRMA() {
-        alert('funciona')
-        return
+        let etapa = etapas[etapaAtual];
+        let votoConfirmado = false;
+
+        if (branco === true) {
+            votos.push({
+                etapa: etapas[etapaAtual].titulo,
+                voto: 'brnaco'
+            })
+            let votoConfirmado = true;
+            console.log('Confirmado como BRANCO...');
+        } else if (numero.length === etapa.numeros) {
+            votoConfirmado = true;
+            votos.push({
+                etapa: etapas[etapaAtual].titulo,
+                voto: numero
+            })
+            console.log('Confirmado como' + ' ' + numero);
+        }
+        if (votoConfirmado) {
+            etapaAtual++;
+            if (etapas[etapaAtual] !== undefined) {
+                initionEtapas();
+                console.log('FIM...');
+            } else {
+                document.querySelector('.tela').innerHTML = `<div class="aviso--gigante pisca">FIM...</div>`;
+                console.log(votos);
+
+            }
+        }
 
     },
 
     CORRIGE() {
-        alert('CORRIGI');
+        initionEtapas()
     },
     BRANCO() {
-        alert('BRANCO');
+        if (numero === '') {
+            votoConfirmado = true;
+            branco = true;
+            seuVotoPara.style.display = 'block';
+            aviso.style.display = 'block';
+            numeros.innerHTML = '';
+            descricao.innerHTML = `<div class="aviso--grande pisca">VOTO EM BRANCO</div>`;
+        }
     }
 }
 
@@ -67,7 +104,28 @@ const update = () => {
             return false
         }
     });
-    console.log('canditado', canditado)
+    if (canditado.length > 0) {
+        canditado = canditado[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${canditado.nome}<br/>Partido: ${canditado.partido}`;
+
+        let fotosHtml = ``
+        for (let i in canditado.fotos) {
+            if (canditado.fotos[i].small) {
+                fotosHtml += ` <div class="d-1-image small"><img src="images/${canditado.fotos[i].url}" alt="" />${canditado.fotos[i].legenda}</div> `;
+
+            } else {
+
+                fotosHtml += ` <div class="d-1-image"><img src="images/${canditado.fotos[i].url}" alt="" />${canditado.fotos[i].legenda}</div> `;
+            }
+        }
+        lateral.innerHTML = fotosHtml;
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `<div class="aviso--grande pisca">VOTO NULO</div>`;
+    }
 
 
 
